791e6f85a4dad3b0f0218307aecd44bcfdd187f4
Briceida <briceidamariscal@gmail.com>
2016-11-07 11:07:13 -0800

    Added views to create/edit blog post inside the site.

--- djangogirlsenv/mysite/urls.py
+++ djangogirlsenv/mysite/urls.py
@@ -14,12 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib import admin
-from django.conf.urls import include
-
+from blog import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'', include('blog.urls')),
-
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
 ]
